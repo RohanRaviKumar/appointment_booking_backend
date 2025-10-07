23b9c624019880984d924dfc719479ef091560da
Shaun-Allan <shaunallan0605@gmail.com>
2025-10-07 13:37:24 +0530

    Initial commit

---
+++ src/main/java/com/example/appointment_booking_backend/AppointmentBookingBackendApplication.java
@@ -0,0 +1,25 @@
+package com.example.appointment_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@RestController
+public class AppointmentBookingBackendApplication {
+
+	public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(AppointmentBookingBackendApplication.class, args);
+        Sample s = context.getBean(Sample.class);
+        s.print();
+	}
+
+//    @RequestMapping("/")
+//    public String hello() {
+//        return "Hello World";
+//    }
+
+}
---
+++ src/main/java/com/example/appointment_booking_backend/Sample.java
@@ -0,0 +1,11 @@
+package com.example.appointment_booking_backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+    public void print(){
+        System.out.println("Appointment Booking Project");
+    }
+}
